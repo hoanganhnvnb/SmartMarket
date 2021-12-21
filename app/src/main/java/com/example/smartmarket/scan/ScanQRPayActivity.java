7ffc6d754aa18b58df01a1606c7731bf115eba5f
hoanganhnvnb <71348563+hoanganhnvnb@users.noreply.github.com>
2021-12-22 00:44:52 +0700

    add img to dialog

---
+++ app/src/main/java/com/example/smartmarket/scan/ScanQRPayActivity.java
@@ -0,0 +1,16 @@
+package com.example.smartmarket.scan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartmarket.R;
+
+public class ScanQRPayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qrpay);
+    }
+}
